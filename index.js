e3b81dc921c25eb0342dd9fc3e0a06d6b584033d
Jordan Nahm <jordannahm7@gmail.com>
2017-09-25 23:34:57 -0400

    i don't care anymore

--- index.js
+++ index.js
@@ -128,9 +128,3 @@ router.get('/', function (req, res) {//front page
     });
 
 });*/
-
-/*router.get("/project/1", function (req, res) {
-       
-    return res.render('project1.html');//, project
-
-});*/
